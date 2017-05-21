19aea806d62b6e1aa2bc42d2694166f8c17a4805
Alexandria Okamoto <alexandriamhko@gmail.com>
2017-05-21 15:23:17 +0100

    JS and CSS 1.0

--- src/js/app.js
+++ src/js/app.js
@@ -17,13 +17,37 @@ $(() => {
       correctAnswer: '22'
     }
   ];
+
   const $radiosHere = $('input[type=radio]');
   const $questionHere = $('.questionHere');
   const $labelsHere = $('label');
   const $form = $('form');
+  // const $score = $('.score');
   let playerAnswer = null;
   let correctAnswer = null;
 
+
+  //
+  // function currentScores() {
+  // currentScore = score;
+  // $score.text(currentScore);
+  // }
+  // startScore();
+  // score = 0;
+  // $score.text(score);
+
+  // function startGame() {
+  //   $instructions.addClass('hidden');
+  //   $gameOver.addClass('hidden');
+  //   $gameBoard.removeClass('hidden');
+  //   $scrambledWord.text(shuffleWord(words[currentRound]));
+  //
+  //   highScores();
+  //   score = 0;
+  //   $score.text(score);
+  // }
+
+  //function generate questions & answers
   function generateQuestionsAnswers() {
     const questionSelected = allQuestions[Math.floor(Math.random()*allQuestions.length)];
     correctAnswer = questionSelected.correctAnswer;
@@ -36,14 +60,10 @@ $(() => {
         $labelsHere.eq(i).html(answers);
         $radiosHere.eq(i).val(answers);
       });
-
     });
   }
 
-  generateQuestionsAnswers();
-
-  $form.on('submit', submitAnswer);
-
+//function submit answer
   function submitAnswer(e) {
     e.preventDefault();
     playerAnswer = $('input[type=radio]:checked').val();
@@ -51,13 +71,20 @@ $(() => {
     checkAnswer();
   }
 
+//function check answer
+
   function checkAnswer() {
     if (playerAnswer === correctAnswer) {
       alert('Correct!');
+      // score += 1;
+      // $score.html(score);
       generateQuestionsAnswers();
     } else {
       alert('Too bad...');
+      location.reload();
     }
   }
+
+  $form.on('submit', submitAnswer);
 
 });
