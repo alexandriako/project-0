8817e5355a175ff17847e6afdeeb57ef9ed089d3
Alexandria Okamoto <alexandriamhko@gmail.com>
2017-05-25 14:24:00 +0100

    timer bug fixed, finished

--- src/js/app.js
+++ src/js/app.js
@@ -63,12 +63,12 @@ $(() => {
 
   function storage() {
     $body.removeClass('start');
-    
     $body.addClass('lose');
     $gameOver.removeClass('hidden');
     audio2.pause();
     audio4.play();
     $gameBoard.addClass('hidden');
+    timer().stop;
     currentRound = 0;
   }
 
@@ -153,6 +153,7 @@ $(() => {
       $gameBoard.addClass('hidden');
       $heading.html('Congrats!');
       $win.removeClass('hidden');
+      timer().stop;
       currentRound = 0;
     } else {
       generateQuestionsAnswers();
